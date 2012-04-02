f5cf42f31de7362cd35326c8de77d78918ce8db6
karthikabinav <karthikabinavs@gmail.com>
2012-04-03 00:22:25 +0530

    A basic file reading to draw the basket

--- Keyboard.asm
+++ Keyboard.asm
@@ -5,7 +5,18 @@
 
 key db 'b'
 pixel_row dw 1
-pixel_col dw 2 dup(1)
+pixel_col dw 1
+
+basket_file db "d:\basket.txt",0
+file_handle dw ?
+buffer dw ?
+
+basket_x dw 0
+basket_y dw 0
+
+cur_x dw 50
+cur_y dw 300
+
 .code
 
 mov_cursor macro row,col 
@@ -44,13 +55,13 @@ START:
     mov ah,0
     int 10h
     
+    call drawBasket    
     check:
         mov ah,01h
         mov al,00h
         int 16h
         
-        
-        DrawPixel 0100b 
+         
     
     jnz check
 
@@ -77,6 +88,50 @@ START:
         ret
 
     printChar endp
+    
+    drawBasket proc near
+        mov al,0        
+        mov dx,offset basket_file
+        mov ah,3dh
+        int 21h
+        
+        jc ret_
+        mov file_handle,ax ; file handle
+        
 
+        read:
+            mov bx,file_handle    
+            mov dx,offset buffer
+            mov al,0
+            mov cx,1
+            mov ah,3Fh
+            int 21h
+
+            mov dx,buffer 
+            cmp dx,'1'
+                jne newline
+            
+            
+            
+            DrawPixel 1111b
+
+            jmp cont
+            newline:
+                cmp dx,10
+                    jne cont
+                
+                mov dx,basket_x
+                mov pixel_col,dx
+                inc pixel_row
+
+            cont:
+            inc pixel_col
+            cmp ax,0
+                jne read
+        
+
+        ret_:
+        ret
+    drawBasket endp
 
 end START
